a0fc4d55cc16be765c16410caed0a8f572e7d936
victor Muriithi <victornjiru80@gmail.com>
2025-03-22 18:22:43 +0300

    a to do list app in react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import './todo.jsx'
+import ToDoList from './todo.jsx'
+
+function App() {
+
+
+  return (
+    <ToDoList/>
+  )
+}
+
+export default App
